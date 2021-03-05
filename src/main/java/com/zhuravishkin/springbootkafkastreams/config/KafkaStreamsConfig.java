7c72640e7f2b23976fd0b95b573c3f3f51739ef1
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-03-06 00:26:44 +0300

    refactored KafkaStreamsConfig

--- src/main/java/com/zhuravishkin/springbootkafkastreams/config/KafkaStreamsConfig.java
+++ src/main/java/com/zhuravishkin/springbootkafkastreams/config/KafkaStreamsConfig.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhuravishkin.springbootkafkastreams.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
@@ -25,19 +27,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@Configuration
 @EnableKafka
 @EnableKafkaStreams
+@Configuration
 public class KafkaStreamsConfig {
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
     public KafkaStreamsConfiguration kStreamsConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testStreams");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-        return new KafkaStreamsConfiguration(props);
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streamsApplicationId");
+        properties.put(StreamsConfig.CLIENT_ID_CONFIG, "streamsClientId");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "streamsGroupId");
+
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
+        properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndContinueExceptionHandler.class.getName());
+
+        properties.put(StreamsConfig.TOPOLOGY_OPTIMIZATION, StreamsConfig.OPTIMIZE);
+        properties.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "1");
+
+        return new KafkaStreamsConfiguration(properties);
     }
 
     @Bean
@@ -57,6 +67,7 @@ public class KafkaStreamsConfig {
         KStream<String, User> userStream =
                 stream.mapValues(this::getUserFromString);
         userStream.to("out-topic", Produced.with(Serdes.String(), userSerde()));
+
         return stream;
     }
 
@@ -67,6 +78,7 @@ public class KafkaStreamsConfig {
         } catch (JsonProcessingException e) {
             log.error(e.getMessage(), e);
         }
+
         return user;
     }
 }
